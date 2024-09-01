fe09bd4dfc03fa31b1a9eaf9827f4116bf68e2be
josh671 <49129783+josh671@users.noreply.github.com>
2024-09-01 10:17:25 -0700

    adding gameboard allowing for state updating while maintaining immutability

---
+++ src/Components/GameBoard.jsx
@@ -0,0 +1,35 @@
+import { useState } from "react"
+
+
+const initialGameBoard = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null],
+]
+export default function GameBoard() {
+    const [gameBoard, setGameBoard] = useState(initialGameBoard); 
+
+    // creating states/objects and updating them in an immutable way. 
+    function handleSelectSquare(rowIndex, colIndex){
+        setGameBoard((prevGameBoard)=>{
+            const updatedBoard = [...prevGameBoard.map(innerArray=>[...innerArray])]; 
+            updatedBoard[rowIndex][colIndex]  = 'X'; 
+            return updatedBoard; 
+        }); 
+    }
+  return (
+  <ol id="game-board">
+    {gameBoard.map((row, rowIndex) => (
+      <li key={rowIndex}>
+        <ol>
+          {row.map((playerSymbol, colIndex) => (
+            <li key={colIndex}>
+              <button onClick={()=>handleSelectSquare(rowIndex, colIndex)}>{playerSymbol}</button>
+            </li>
+          ))}
+        </ol>
+      </li>
+    ))}
+  </ol>
+  )
+}
